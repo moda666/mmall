ddf00fce13ffee9704e04d3f98e9676bb214b249
adXiang <1163978683@qq.com>
2018-01-04 20:34:27 +0800

    2018/1/4 更新

---
+++ src/main/java/com/mmall/service/IShippingService.java
@@ -0,0 +1,22 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+
+/**
+ * @author MaWenXiang
+ * @date 2018/1/4
+ */
+public interface IShippingService {
+
+    ServerResponse add(Integer userId, Shipping shipping);
+
+    ServerResponse<String> del(Integer userId, Integer shippingId);
+
+    ServerResponse update(Integer userId, Shipping shipping);
+
+    ServerResponse<Shipping> select(Integer userId, Integer shippingId);
+
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+}
