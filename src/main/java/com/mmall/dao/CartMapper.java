aae7b8587ad1af5789567d0a33201bb1c164fab3
adXiang <1163978683@qq.com>
2018-01-18 20:05:16 +0800

    2018/1/18 更新

--- src/main/java/com/mmall/dao/CartMapper.java
+++ src/main/java/com/mmall/dao/CartMapper.java
@@ -29,4 +29,6 @@ public interface CartMapper {
     int checkedOrUncheckedProduct(@Param("userId") Integer userId, @Param("productId") Integer productId, @Param("checked") Integer checked);
 
     int selectCartProductCount(Integer userId);
+
+    List<Cart> selectCheckedCartByUserId(Integer userId);
 }
